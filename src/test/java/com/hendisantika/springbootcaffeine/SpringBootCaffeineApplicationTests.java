71bab0b7de27e91777a6c15fd0f875781cab704d
Hendi Santika <hendisantika@gmail.com>
2020-01-23 05:54:52 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/springbootcaffeine/SpringBootCaffeineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootcaffeine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCaffeineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
