c906fde4f2778159b68c6c7bd8c858e30e2d952b
Hendi Santika <hendisantika@gmail.com>
2020-01-23 06:19:03 +0700

    #7 - Modify Main App

--- src/main/java/com/hendisantika/springbootcaffeine/SpringBootCaffeineApplication.java
+++ src/main/java/com/hendisantika/springbootcaffeine/SpringBootCaffeineApplication.java
@@ -1,13 +1,35 @@
 package com.hendisantika.springbootcaffeine;
 
+import com.hendisantika.springbootcaffeine.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
-public class SpringBootCaffeineApplication {
+public class SpringBootCaffeineApplication implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(SpringBootCaffeineApplication.class);
+
+    @Autowired
+    CustomerService customerService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCaffeineApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        LOG.info("Starting the Caffeine cache testing process");
+        customerService.getCustomer(1l); //No hit , since this is the first request.
+        customerService.getCustomer(2l); //No hit , since this is the first request.
+        customerService.getCustomer(1l); //hit , since it is already in the cache.
+        customerService.getCustomer(1l); //hit , since it is already in the cache.
+        customerService.getCustomer(1l); //hit , since it is already in the cache.
+        customerService.getCustomer(1l); //hit , since it is already in the cache.
+    }
 }
